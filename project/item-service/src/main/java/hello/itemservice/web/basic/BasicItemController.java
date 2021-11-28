0f522369254604c16f5e7def53b68b1cf3c25dc7
seriousKang <seriousKang@gmail.com>
2021-11-28 11:17:53 +0900

    Product registration form

--- project/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ project/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -3,15 +3,15 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
@@ -44,6 +44,19 @@ public class BasicItemController {
     public String item(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute Item item, Model model) {
+        Item savedItem = itemRepository.save(item);
+        model.addAttribute("item", savedItem);
 
         return "basic/item";
     }
