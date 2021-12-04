59ee39b644a5d0b7d87689dc0c4996e075e9aa18
seriousKang <scdean110@gmail.com>
2021-12-04 19:29:42 +0900

    comments

--- project/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ project/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -103,6 +103,12 @@ public class BasicController {
         return "basic/condition";
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "/basic/comments";
+    }
+
 
     @Component("helloBean")
     static class HelloBean {
