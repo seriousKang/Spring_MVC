888b09af694e4edcd8eb92de1fbb2373bf022db0
seriousKang <scdean110@gmail.com>
2021-12-04 11:28:54 +0900

    repetition

--- project/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ project/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -87,8 +87,14 @@ public class BasicController {
     }
 
     @GetMapping("/attribute")
-    public String attribte() {
+    public String attribute() {
         return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
     }
 
 
@@ -108,5 +114,14 @@ public class BasicController {
             this.username = username;
             this.age = age;
         }
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
     }
 }
