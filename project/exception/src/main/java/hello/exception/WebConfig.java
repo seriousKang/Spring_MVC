fe2bc2b3de8d7bedf5ee478068ca560d7d312965
seriousKang <scdean110@gmail.com>
2021-12-26 21:55:29 +0900

    servlet exception handling - filter

---
+++ project/exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,29 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        // 클라이언트 요청뿐만 아니라, 오류 페이지 요청에도 필터 호출
+        //   - 아무 값도 넣지 않을 경우 `DispatcherType.REQUEST`
+        //   - 즉 오류 페이지 경로도 필터 적용할 것이 아니면, 기본 값 그대로 사용하면 됨
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+
+        return filterRegistrationBean;
+    }
+}
